a7287192c558d5a1e3d13368f74714087617124e
Ivan Wilhelm <contato@ivanwilhelm.dev>
2025-03-30 10:04:32 -0300

    feat: Implement DeleteUserRoute and DeleteUserResponseDto for user deletion functionality

---
+++ src/infrastructure/api/routers/users/delete_user/delete_user_response_dto.dart
@@ -0,0 +1,48 @@
+import 'package:uuid/uuid.dart';
+
+import '../../../../../usecase/users/delete_user/delete_user_output_dto.dart';
+
+final class DeleteUserResponseDto {
+  final UuidValue _id;
+  final String _firstName;
+  final String _lastName;
+  final String _email;
+  final DateTime _createdAt;
+  final DateTime? _updatedAt;
+
+  const DeleteUserResponseDto({
+    required UuidValue id,
+    required String firstName,
+    required String lastName,
+    required String email,
+    required DateTime createdAt,
+    DateTime? updatedAt,
+  }) : _id = id,
+       _firstName = firstName,
+       _lastName = lastName,
+       _email = email,
+       _createdAt = createdAt,
+       _updatedAt = updatedAt;
+
+  factory DeleteUserResponseDto.fromOutputDto(DeleteUserOutputDto output) {
+    return DeleteUserResponseDto(
+      id: output.id,
+      firstName: output.firstName,
+      lastName: output.lastName,
+      email: output.email,
+      createdAt: output.createdAt,
+      updatedAt: output.updatedAt,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': _id.uuid,
+      'firstName': _firstName,
+      'lastName': _lastName,
+      'email': _email,
+      'createdAt': _createdAt.toIso8601String(),
+      'updatedAt': _updatedAt?.toIso8601String(),
+    };
+  }
+}
---
+++ src/infrastructure/api/routers/users/delete_user/delete_user_route.dart
@@ -0,0 +1,60 @@
+import 'package:logging/logging.dart';
+import 'package:postgres/postgres.dart';
+import 'package:shelf/shelf.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/validation.dart';
+
+import '../../../../../usecase/users/delete_user/delete_user_input_dto.dart';
+import '../../../../../usecase/users/delete_user/delete_user_usecase.dart';
+import '../../../../database/users/users_repository_impl.dart';
+import '../../server_instance.dart';
+import '../../shared/core_router.dart';
+import '../../shared/json_response.dart';
+import 'delete_user_response_dto.dart';
+
+final class DeleteUserRoute implements CoreRouter {
+  final Logger _logger;
+  final ServerInstance _instance;
+  final Connection _dbconn;
+
+  DeleteUserRoute({
+    required ServerInstance instance,
+    required Connection dbconn,
+  }) : _instance = instance,
+       _dbconn = dbconn,
+       _logger = Logger('DeleteUserRoute');
+
+  @override
+  Future<void> configure() async {
+    final route = '/${_instance.apiVersion}/users/<userId>';
+    _instance.app.delete(route, _deleteUser);
+
+    _logger.info('Registering route: DELETE $route');
+  }
+
+  Future<Response> _deleteUser(Request request, String userId) async {
+    _logger.info('Deleting user with ID: $userId');
+
+    if (!UuidValidation.isValidUUID(fromString: userId)) {
+      return JsonResponse.badRequest('User ID must be a valid UUID.');
+    }
+
+    final repository = UsersRepositoryImpl(connection: _dbconn);
+    final usecase = DeleteUserUsecase(repository: repository);
+    final input = DeleteUserInputDto(id: UuidValue.fromString(userId));
+    final deleted = await usecase.execute(input);
+
+    return deleted.fold(
+      ifLeft: (exception) {
+        _logger.severe('Error deleting user: ${exception.businessMessage}');
+        return JsonResponse.internalServerError(exception.businessMessage);
+      },
+      ifRight: (output) {
+        _logger.info('User deleted successfully: ${output.toString()}');
+        return JsonResponse.ok(
+          DeleteUserResponseDto.fromOutputDto(output).toMap(),
+        );
+      },
+    );
+  }
+}
