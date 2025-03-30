7ea464159ce96ecff9f7dfaf3dd3e5f10fa5c0a7
Ivan Wilhelm <contato@ivanwilhelm.dev>
2025-03-30 10:45:57 -0300

    refactor: Enhance logging to differentiate between severe and informational messages

--- src/infrastructure/api/commands/configure_logging_command.dart
+++ src/infrastructure/api/commands/configure_logging_command.dart
@@ -7,16 +7,29 @@ abstract class ConfigureLoggingCommand {
   static void configureLogging() {
     Logger.root.level = Level.INFO;
 
-    Logger.root.onRecord.listen((record) {
+    Logger.root.onRecord.listen((logRecord) {
+      if (logRecord.level >= Level.SEVERE) {
+        stderr.writeln(
+          json.encode({
+            'time': logRecord.time.toIso8601String(),
+            'sequenceNumber': logRecord.sequenceNumber,
+            'level': logRecord.level.name,
+            'logger': logRecord.loggerName,
+            'message': logRecord.message,
+            'error': logRecord.error?.toString(),
+            'stackTrace': logRecord.stackTrace?.toString(),
+          }),
+        );
+        return;
+      }
+
       stdout.writeln(
         json.encode({
-          'time': record.time.toIso8601String(),
-          'sequenceNumber': record.sequenceNumber,
-          'level': record.level.name,
-          'logger': record.loggerName,
-          'message': record.message,
-          'error': record.error?.toString(),
-          'stackTrace': record.stackTrace?.toString(),
+          'time': logRecord.time.toIso8601String(),
+          'sequenceNumber': logRecord.sequenceNumber,
+          'level': logRecord.level.name,
+          'logger': logRecord.loggerName,
+          'message': logRecord.message,
         }),
       );
     });
